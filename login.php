8659b07f2d7daca969a5c5a73cd3b0c0c5ccc83e
DvncanG <dvncan30@gmail.com>
2023-11-29 19:13:14 +0100

    Login

--- login.php
+++ login.php
@@ -1,100 +1,99 @@
 <!-- login.php -->
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Iniciar sesión</title>
 </head>
-
 <body>
 
-    <?php
-    include("DB.php"); // Incluir el archivo de conexión a la base de datos
-    
-    session_start();
+<?php
+include("DB.php"); // Incluir el archivo de conexión a la base de datos
 
-    // Función para reconectar si es necesario
-    function reconectar()
-    {
-        global $conn, $servername, $username, $password, $dbname;
+session_start();
 
-        if ($conn->ping() === false) {
-            $conn = new mysqli($servername, $username, $password, $dbname);
+// Función para reconectar si es necesario
+function reconectar() {
+    global $conn, $servername, $username, $password, $dbname;
 
-            if ($conn->connect_error) {
-                die("Conexión fallida: " . $conn->connect_error);
-            }
+    if ($conn->ping() === false) {
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        if ($conn->connect_error) {
+            die("Conexión fallida: " . $conn->connect_error);
         }
-
-        return $conn;
     }
 
-    // Verificar si se ha enviado el formulario
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $nombre = $_POST["nombre"];
-        $contrasena = $_POST["contrasena"];
+    return $conn;
+}
 
-        // Reconectar si es necesario
-        $conn = reconectar();
+// Verificar si se ha enviado el formulario
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = $_POST["nombre"];
+    $contrasena = $_POST["contrasena"];
 
-        // Utilizar una sentencia preparada para evitar la inyección SQL
-        $sql = "SELECT * FROM usuarios WHERE nombre = ? AND contrasena = ?";
-        $stmt = $conn->prepare($sql);
+    // Reconectar si es necesario
+    $conn = reconectar();
 
-        if (!$stmt) {
-            die("Error en la preparación de la consulta: " . $conn->error);
-        }
+    // Utilizar una sentencia preparada para evitar la inyección SQL
+    $sql = "SELECT * FROM usuarios WHERE nombre = ? AND contrasena = ?";
+    $stmt = $conn->prepare($sql);
 
-        $stmt->bind_param("ss", $nombre, $contrasena);
-        $stmt->execute();
-        $result = $stmt->get_result();
+    if (!$stmt) {
+        die("Error en la preparación de la consulta: " . $conn->error);
+    }
 
-        if (!$result) {
-            die("Error en la ejecución de la consulta: " . $stmt->error);
-        }
+    $stmt->bind_param("ss", $nombre, $contrasena);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-        if ($result->num_rows > 0) {
-            $row = $result->fetch_assoc();
+    if (!$result) {
+        die("Error en la ejecución de la consulta: " . $stmt->error);
+    }
 
-            // Configurar la cookie de sesión para que sea persistente durante 30 días (ajusta según tus necesidades)
-            $expire = time() + (6 * 60 * 60); // 6 horas
-            setcookie(session_name(), session_id(), $expire, "/");
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
 
-            $_SESSION["rol"] = $row["rol"];
+        // Configurar la cookie de sesión para que sea persistente durante 6 horas
+        $expire = time() + (6 * 60 * 60); // 6 horas
+        setcookie(session_name(), session_id(), $expire, "/");
 
-            if ($_SESSION["rol"] == "admin") {
-                header("Location: pagina_admin.php");
-                exit(); // Asegura que el script se detenga después de la redirección
-            } elseif ($_SESSION["rol"] == "usuario") {
-                header("Location: pagina_usuario.php");
-                exit(); // Asegura que el script se detenga después de la redirección
-            } else {
-                echo "Rol no válido";
-            }
+        // Configurar una cookie para recordar el nombre de usuario
+        setcookie("usuario", $nombre, $expire, "/");
+
+        $_SESSION["rol"] = $row["rol"];
+
+        if ($_SESSION["rol"] == "admin") {
+            header("Location: pagina_admin.php");
+            exit(); // Asegura que el script se detenga después de la redirección
+        } elseif ($_SESSION["rol"] == "usuario") {
+            header("Location: pagina_usuario.php");
+            exit(); // Asegura que el script se detenga después de la redirección
         } else {
-            echo "Credenciales incorrectas";
+            echo "Rol no válido";
         }
-
-        // Cerrar la sentencia preparada
-        $stmt->close();
+    } else {
+        echo "Credenciales incorrectas";
     }
 
-    // No cerramos explícitamente la conexión aquí, dejamos que PHP maneje la liberación de recursos al final del script
-    ?>
+    // Cerrar la sentencia preparada
+    $stmt->close();
+}
 
-    <h2>Iniciar sesión</h2>
-    <form action="login.php" method="post">
-        <label for="nombre">Nombre:</label>
-        <input type="text" id="nombre" name="nombre" required><br>
+// No cerramos explícitamente la conexión aquí, dejamos que PHP maneje la liberación de recursos al final del script
+?>
 
-        <label for="contrasena">Contraseña:</label>
-        <input type="password" id="contrasena" name="contrasena" required><br>
+<h2>Iniciar sesión</h2>
+<form action="login.php" method="post">
+    <label for="nombre">Nombre:</label>
+    <input type="text" id="nombre" name="nombre" required value="<?php echo isset($_COOKIE['usuario']) ? $_COOKIE['usuario'] : ''; ?>"><br>
 
-        <input type="submit" value="Iniciar sesión">
-    </form>
+    <label for="contrasena">Contraseña:</label>
+    <input type="password" id="contrasena" name="contrasena" required><br>
+
+    <input type="submit" value="Iniciar sesión">
+</form>
 
 </body>
-
 </html>
